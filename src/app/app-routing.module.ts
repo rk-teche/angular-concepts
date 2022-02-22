e141e5ed757f3f1d678d3c5d4a3d2a14664860c9
rk-teche <porwalcs019@gmail.com>
2022-02-22 11:47:00 +0530

    Angular module created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   { path: 'html', loadChildren: () => import('./html/html.module').then(m => m.HtmlModule) },
   { path: 'css', loadChildren: () => import('./css/css.module').then(m => m.CssModule) },
   { path: 'javascript', loadChildren: () => import('./javascript/javascript.module').then(m => m.JavascriptModule)  },
+  { path: 'angular', loadChildren: () => import('./angular/angular.module').then(m => m.AngularModule) },
 ];
 
 @NgModule({
