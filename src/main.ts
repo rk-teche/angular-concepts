ace858db591ed4f993bc45344248cc75516fff61
rk-teche <porwalcs019@gmail.com>
2022-02-24 22:37:08 +0530

    resolve complier error

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
+import '@angular/compiler'; //https://github.com/angular/angular-cli/issues/16873
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
